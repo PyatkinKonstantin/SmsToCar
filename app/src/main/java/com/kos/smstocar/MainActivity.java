d8b3027b92cdc758f372c6ff4ce8d2f3b9bd889e
Kos <ptkn@yandex.ru>
2022-09-28 23:39:40 +0300

    1

---
+++ app/src/main/java/com/kos/smstocar/MainActivity.java
@@ -0,0 +1,49 @@
+package com.kos.smstocar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+
+        getPermission();
+    }
+
+    protected void getPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        0);
+            }
+        }
+    }
+
+    public void send20(View view) {
+        //SmsManager.getDefault()
+          //      .sendTextMessage("+79195043449", null, "?*", null, null);
+        String t20 = "Запущено на 20 минут";
+        text.setText(t20);
+        Toast.makeText(this, t20, Toast.LENGTH_LONG).show();
+    }
+}
